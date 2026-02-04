956eb669e1bbd45ba8b20d0b2d47de14b801b565
Simon Jakobi <simon.jakobi@gmail.com>
2026-02-04 13:28:08 +0100

    Implement lookup and insert with QuickCheck tests

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -2,13 +2,17 @@ module MyLib
   ( Word64Map
   , empty
   , insert
+  , lookup
   ) where
 
+import Prelude hiding (lookup)
+import Data.Bits hiding (shift, bit)
+import qualified Data.Bits as Bits
 import Data.Word (Word64)
-import Data.Primitive.SmallArray (SmallArray)
+import Data.Primitive.SmallArray
 
 data Word64Map a =
-    Branch Bitmap (SmallArray (Word64Map a))
+    Branch !Bitmap !(SmallArray (Word64Map a))
   | Leaf !Word64 a
 
 newtype Bitmap = BM Word64
@@ -17,5 +21,64 @@ empty :: Word64Map a
 empty = Branch (BM 0) mempty
 {-# noinline empty #-}
 
+lookup :: Word64 -> Word64Map a -> Maybe a
+lookup k = go 0
+  where
+    go _ (Leaf k' v)
+      | k == k' = Just v
+      | otherwise = Nothing
+    go shift (Branch (BM bm) ary) =
+      let idx = fromIntegral ((k `Bits.shiftR` shift) .&. 0x3f)
+          bit = 1 `Bits.shiftL` idx
+      in if bm .&. bit == 0
+         then Nothing
+         else let i = popCount (bm .&. (bit - 1))
+              in go (shift + 6) (indexSmallArray ary i)
+
 insert :: Word64 -> a -> Word64Map a -> Word64Map a
-insert = undefined
+insert k v m = go 0 m
+  where
+    go shift (Leaf k' v')
+      | k == k' = Leaf k v
+      | otherwise = split shift k v k' v'
+    go shift (Branch (BM bm) ary) =
+      let idx = fromIntegral ((k `Bits.shiftR` shift) .&. 0x3f)
+          bit = 1 `Bits.shiftL` idx
+      in if bm .&. bit == 0
+         then let i = popCount (bm .&. (bit - 1))
+              in Branch (BM (bm .|. bit)) (insertAt i (Leaf k v) ary)
+         else let i = popCount (bm .&. (bit - 1))
+                  child = indexSmallArray ary i
+                  newChild = go (shift + 6) child
+              in Branch (BM bm) (updateAt i newChild ary)
+
+    split shift k1 v1 k2 v2 =
+      let idx1 = fromIntegral ((k1 `Bits.shiftR` shift) .&. 0x3f)
+          idx2 = fromIntegral ((k2 `Bits.shiftR` shift) .&. 0x3f)
+      in if idx1 /= idx2
+         then
+           let bm = (1 `Bits.shiftL` idx1) .|. (1 `Bits.shiftL` idx2)
+               ary = if idx1 < idx2
+                     then smallArrayFromList [Leaf k1 v1, Leaf k2 v2]
+                     else smallArrayFromList [Leaf k2 v2, Leaf k1 v1]
+           in Branch (BM bm) ary
+         else
+           let child = split (shift + 6) k1 v1 k2 v2
+               bm = 1 `Bits.shiftL` idx1
+           in Branch (BM bm) (smallArrayFromList [child])
+
+insertAt :: Int -> a -> SmallArray a -> SmallArray a
+insertAt i a ary = runSmallArray $ do
+  let n = sizeofSmallArray ary
+  mary <- newSmallArray (n + 1) a
+  copySmallArray mary 0 ary 0 i
+  copySmallArray mary (i + 1) ary i (n - i)
+  return mary
+
+updateAt :: Int -> a -> SmallArray a -> SmallArray a
+updateAt i a ary = runSmallArray $ do
+  let n = sizeofSmallArray ary
+  mary <- newSmallArray n a
+  copySmallArray mary 0 ary 0 n
+  writeSmallArray mary i a
+  return mary
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
 
+import Test.QuickCheck
+import qualified Data.Map.Strict as Map
+import MyLib (empty, insert, lookup)
+import Prelude hiding (lookup)
+import Data.Word (Word64)
+
+prop_lookup :: [(Word64, Int)] -> [Word64] -> Bool
+prop_lookup entries keys =
+  let myMap = foldl' (\m (k, v) -> insert k v m) empty entries
+      refMap = Map.fromList entries
+  in all (\k -> lookup k myMap == Map.lookup k refMap) (keys ++ map fst entries)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = quickCheck prop_lookup

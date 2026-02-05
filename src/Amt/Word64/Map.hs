f82c47c3c25d6f6bdbebec39e31182ee5d0f1d93
Simon Jakobi <simon.jakobi@gmail.com>
2026-02-05 11:23:37 +0100

    Inline collapsing logic in unionAtShift

--- src/Amt/Word64/Map.hs
+++ src/Amt/Word64/Map.hs
@@ -414,7 +414,13 @@ unionAtShift shift m1 m2 = case (m1, m2) of
                 (Just i1, Nothing) -> indexSmallArray ary1 i1
                 (Nothing, Just i2) -> indexSmallArray ary2 i2
                 (Nothing, Nothing) -> error "union: impossible"
-     in mkBranch (BM newBm) (smallArrayFromList newAryList)
+        newAry = smallArrayFromList newAryList
+     in case sizeofSmallArray newAry of
+          0 -> empty
+          1 -> case indexSmallArray newAry 0 of
+            l@Leaf{} -> l
+            _ -> Branch (BM newBm) newAry
+          _ -> Branch (BM newBm) newAry
 
 unionWith :: (a -> a -> a) -> Word64Map a -> Word64Map a -> Word64Map a
 unionWith f = unionWithKey (\_ x y -> f x y)

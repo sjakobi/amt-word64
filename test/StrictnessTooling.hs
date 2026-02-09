5b6aa238a1b98cfe33d4ed4304b206b2513dbb3a
Simon Jakobi <simon.jakobi@gmail.com>
2026-02-09 20:08:00 +0100

    Add failing strictness test for Strict.insert

--- test/StrictnessTooling.hs
+++ test/StrictnessTooling.hs
@@ -3,9 +3,14 @@ module StrictnessTooling
   , tests
   ) where
 
+import Amt.Word64.Map.Strict qualified as Strict
+import Control.DeepSeq (deepseq)
 import Control.Exception (SomeException, try)
 import Data.Maybe (isNothing)
+import Data.Word (Word64)
+import GHC.Exts (lazy)
 import NoThunks.Class (ThunkInfo, noThunks)
+import Test.QuickCheck (Property)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.QuickCheck (ioProperty, testProperty)
 
@@ -27,4 +32,23 @@ tests =
     , testProperty "forced Int is WHNF" $
         ioProperty $
           isWhnfInt (42 :: Int)
+    , testProperty "Strict.insert forces WHNF values" $
+        prop_strict_insert_whnf_values
     ]
+
+prop_strict_insert_whnf_values :: [(Word64, Int)] -> Word64 -> Int -> Property
+prop_strict_insert_whnf_values entries k v = ioProperty $ do
+  let m0 = Strict.fromList entries
+  m0 `deepseq` pure ()
+  let vThunk = mkThunk v
+  preOk <- isWhnfInt vThunk
+  if preOk
+    then error "Expected a thunk for Strict.insert input, but got WHNF"
+    else do
+      let m1 = Strict.insert k vThunk m0
+      oks <- traverse (isWhnfInt . snd) (Strict.toList m1)
+      pure (and oks)
+
+{-# NOINLINE mkThunk #-}
+mkThunk :: Int -> Int
+mkThunk x = lazy x
